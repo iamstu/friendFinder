23e3a87b090dbda6fb40d26651e18a19d91d532d
unknown <stuartalafollette@gmail.com>
2018-07-04 12:19:16 -0500

    get route functional without proper content

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(express.static('public'));
+
+
+require("./app/routing/htmlRoutes.js")(app);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
